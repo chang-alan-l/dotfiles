f9e9d68210e866a75739f9675f128ae3898c504e
Alan Chang <25752490+chang-alan-l@users.noreply.github.com>
2022-12-30 01:40:47 -0800

    Fix tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,4 +3,4 @@
 set -e
 
 ln -sf "${DOTFILES_LOCATION}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
-ln -sf "${DOTFILES_LOCATION}/tmux/tmux.conf.local" "${HOME}/tmux.conf.local"
+ln -sf "${DOTFILES_LOCATION}/tmux/.tmux.conf.local" "${HOME}/.tmux.conf.local"
