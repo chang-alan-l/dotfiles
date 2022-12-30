927b7dba73d8726b72595c6d18edceaac94ca625
Alan Chang <25752490+chang-alan-l@users.noreply.github.com>
2022-12-30 00:53:14 -0800

    Also remove github dotfiles from the install all script for now.

--- install.sh
+++ install.sh
@@ -12,12 +12,16 @@ export DOTFILES_LOCATION;
 # Install dependencies
 ###
 ./bin/dotfiles install zsh
-# This is probably better manually installed, since we will default to zsh
-# typically
-# ./bin/dotfiles install fish
-# Requires input, harder to do inside container
-# ./bin/dotfiles install git
-./bin/dotfiles install github
 ./bin/dotfiles install starship
 ./bin/dotfiles install tmux
 ./bin/dotfiles install nvim
+
+# This is probably better manually installed, since we will default to zsh
+# typically
+# ./bin/dotfiles install fish
+
+# Requires input, harder to do inside container
+# ./bin/dotfiles install git
+# ./bin/dotfiles install github
+
+
